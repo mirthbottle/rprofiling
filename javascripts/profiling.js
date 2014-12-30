851a2834a08b1bcbaf77f8d84d48fad5fbb249f2
mirthbottle <mirthbottle@gmail.com>
2014-12-30 00:48:11 -0800

    renamed variables in profiling.js

--- javascripts/profiling.js
+++ javascripts/profiling.js
@@ -1,49 +1,42 @@
-function conduct_search() {
-  var b_shoppers = document.getElementById('b_shoppers').value;
-  var b_shoplifters = document.getElementById('b_shoplifters').value;
-  var w_shoplifters = document.getElementById('w_shoplifters').value;
-  var b_caught = document.getElementById('b_caught').value;
-  var w_caught = document.getElementById('w_caught').value;
-
-  var b_s = b_shoplifters/100.0*b_shoppers;
-  var b_i = b_shoppers - b_s;
-  var w_s = w_shoplifters/100.0*(100-b_shoppers);
-  var w_i = 100 - b_shoppers - w_s;
-  var b_c = b_s*b_caught/100.0;
-  var b_ga = b_s - b_c;
-  var w_c = w_s*w_caught/100.0;
-  var w_ga = w_s - w_c;
-
-  caught_bw = 100.0 * b_c/(b_c + w_c);
-  document.getElementById('caught_bw_ratio').innerHTML = caught_bw.toFixed(1);
-  shoplifters_bw = 100.0 * b_s/(b_s + w_s);
-  document.getElementById('shoplifters_bw_ratio').innerHTML = shoplifters_bw.toFixed(1);
-
-  caught_error = (caught_bw/shoplifters_bw - 1)*100;
-  document.getElementById('caught_error').innerHTML = caught_error.toFixed(1);
-  display_shoppers(b_i, b_s, w_i,w_s, b_c, b_ga, w_c, w_ga, caught_bw, shoplifters_bw);
+function apply_filters() {
+  // % of population that is group a
+  var pa = document.getElementById('pa').value;
+  // p(1|a) = % of group a that is group 1
+  var p1_a = document.getElementById('p1_a').value;
+  // p(1|b) = % of group b that is group 1
+  var p1_b = document.getElementById('p1_b').value;
+  // p(f1| (a && 1)) = % of subgroup a1 captured by filter1
+  var pf1_a1 = document.getElementById('pf1_a1').value;
+  // p(f1| (b && 1)) = % of subgroup b1 captured by filter1
+  var pf1_b1 = document.getElementById('pf1_b1').value;
+  
+  // p(a && 1) = % of total that is subgroup a1
+  var pa1 = p1_a/100.0*pa;
+  var pa2 = pa - pa1;
+  // % of total that is subgroup b1
+  var pb1 = p1_b/100.0*(100-pa);
+  var pb2 = 100 - pa - pb1;
+  // p(a && 1 && f1) = % of total that is subgroup a1 and captured by filter1
+  var pa1f1 = pa1*pf1_a1/100.0;
+  var pa2f1 = pa1 - pa1f1;
+  // % of total that is subgroup b1 and captured by filter1
+  var pb1f1 = pb1*pf1_b1/100.0;
+  var pb2f1 = pb1 - pb1f1;
+  
+  // p(a| (1 && f1) = % of group 1 captured by filter1 that are group a
+  pa_1f1 = 100.0 * pa1f1/(pa1f1 + pb1f1);
+  document.getElementById('pa_1f1').innerHTML = pa_1f1.toFixed(1);
+  // % of group 1 that are group a
+  pa_1 = 100.0 * pa1/(pa1 + pb1);
+  document.getElementById('pa_1').innerHTML = pa_1.toFixed(1);
+  
+  // % overrepresentation of ga among sg1 captured by filter1 
+  ea_1f1 = (pa_1f1/pa_1 - 1)*100;
+  document.getElementById('ea_1f1').innerHTML = ea_1f1.toFixed(1);
+  display_shoppers(pa1, pa2, pb1, pb2, pa1f1, pa2f1, pb1f1, pb2f1, pa_1f1, pa_1);
 }
 
-function display_search_results(b_i, b_s, w_i, w_s, b_c, b_ga, w_c, w_ga, c_r, g_r){
-  // c_r = caught ratio
-  // g_r = guilty ratio
-  var scale = 5;
-  document.getElementById('b_ishoppers_bar').style.cssText="width:" + b_i*scale + "px;background-color: orange;";
-  document.getElementById('b_shoplifters_bar').style.cssText="width:" + b_s*scale + "px;background-color: orangered;";
-  document.getElementById('w_ishoppers_bar').style.cssText="width:" + w_i*scale + "px;background-color: yellowgreen;";
-  document.getElementById('w_shoplifters_bar').style.cssText="width:" + w_s*scale + "px;background-color: olivedrab;";
-
-
-  document.getElementById('b_innocent').style.cssText="width:" + b_i*scale + "px;background-color: orange;";
-  document.getElementById('b_got_away').style.cssText="width:" + b_ga*scale + "px;background-color: orangered;";
-  document.getElementById('b_caught_bar').style.cssText="width:" + b_c*scale + "px;background-color: red;";
-  document.getElementById('w_innocent').style.cssText="width:" + w_i*scale + "px;background-color: yellowgreen;";
-  document.getElementById('w_got_away').style.cssText="width:" + w_ga*scale + "px;background-color: olivedrab;";
-  document.getElementById('w_caught_bar').style.cssText="width:" + w_c*scale + "px;background-color: darkolivegreen;";
-
-  document.getElementById('b_caught_races').style.cssText="width:" + c_r*scale + "px;background-color: red;";
-  document.getElementById('w_caught_races').style.cssText="font-size:20px;width:" + (100-c_r)*scale + "px;background-color: darkolivegreen;";
-  document.getElementById('b_guilty').style.cssText="width:" + g_r*scale + "px;background-color: orangered;";
-  document.getElementById('w_guilty').style.cssText="font-size:20px;width:" + (100-g_r)*scale + "px;background-color: olivedrab;";
+function display_bars(pa1, pa2, pb1, pb2, pa1f1, pa2f1, pb1f1, pb2f1, pa_1f1, pa_1){
+  
+  
 }
-
